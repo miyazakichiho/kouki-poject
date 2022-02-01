3e6b3e6ba01bdae9108a1d0504681504c7e145cc
miyazakichiho <zm18455@josai.ac.jp>
2022-02-01 10:00:29 +0000

    0201

---
+++ ex1-1.js
@@ -0,0 +1,26 @@
+function txtchange1() {
+    document.getElementById('txt1').innerHTML= '残念だわん';}
+function txtchange2() {
+    document.getElementById('txt1').innerHTML= 'ざんねんだわん';}
+function txtchange3() {
+    document.getElementById('txt1').innerHTML= 'ザンネンダワン';}
+function txtchange4() {
+    document.getElementById('txt1').innerHTML= '正解だワン！ 解説：推理小説→「す」入り小説';}
+
+
+function txtchange5() {
+    document.getElementById('txt2').innerHTML= 'チガウワン';}
+function txtchange6() {
+    document.getElementById('txt2').innerHTML= 'ピンポーンだワン！　解説：芯出る（死んでる）';}
+function txtchange7() {
+    document.getElementById('txt2').innerHTML= 'ちがうわん';}
+function txtchange8() {
+    document.getElementById('txt2').innerHTML= '違うわん';}
+
+
+function txtchange9() {
+    document.getElementById('txt3').innerHTML= '似てるけど違うワン！';}
+function txtchange10() {
+    document.getElementById('txt3').innerHTML= '正解だワン！';}
+function txtchange11() {
+    document.getElementById('txt3').innerHTML= 'そんなに小さくないワン！';}
