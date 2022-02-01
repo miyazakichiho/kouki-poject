ac224eb61b31520ef3724812924b206c8c0d873f
miyazakichiho <zm18455@josai.ac.jp>
2022-02-01 10:43:11 +0000

    0201

--- ex1-5.js
+++ ex1-5.js
@@ -1,8 +1,3 @@
-function nazo2()
-{if (document.getElementById('answer2').value=='日本')
-{alert('あたりだワン!　解説：「電気をつけろ」と日本語で言っているから');}
-else{alert('はずれだワン！');}}
-
 function nazo3()
 {if (document.getElementById('answer3').value=='3')
 {alert('あたりだワン！よくわかったね！');}
--- ex1-6.js
+++ ex1-6.js
@@ -1,4 +1,4 @@
 function hyoji()
-{ var str = " ①食べること　②テレビを観ること　③寝ること"; var cnt = document.timer.moji.value.length;if ( cnt < 30 ) 
+{ var str = " ①歌を歌うこと　②ショッピング"; var cnt = document.timer.moji.value.length;if ( cnt < 30 ) 
 {document.timer.moji.value = str.substr(0,cnt+1);}else{ document.timer.moji.value = ""; } setTimeout("hyoji()",1000);
 }
